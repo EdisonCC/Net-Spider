aca267dc3986456047a1fe5cf86717bdc841d3aa
FioraLove <3434279505@qq.com>
2021-03-23 16:59:59 +0800

    【快手图集】适配多端

--- 图集/kuai_pic.py
+++ 图集/kuai_pic.py
@@ -12,14 +12,20 @@ from pyquery import PyQuery as pq
     1. 通过APP里的分享获取视频url：https://v.kuaishou.com/cYRkmv
     2. url会经过两次重定向，然后跳转到真实地址
     3. 图片就是在其网页元素img标签中，不是经由异步请求获取的
-    
-  - WEB端：
 
+  - WEB端：
+    web端的快手图集经测试【必须要登录】才可以，若不登录的话，请求返回的结果是没有任何参考价值的
+    url形如：
+      - https://live.kuaishou.com/u/RR1718190/3x4bduuh4uqrbxa?did=web_088cf239f6654844bce30eb5e3c3abca
+      - https://live.kuaishou.com/u/mx278516/3xahgx5ya7u366a?did=web_088cf239f6654844bce30eb5e3c3abca
+            
 """
 
+
 class KuaiPic(object):
-    def __init__(self, url):
+    def __init__(self, url, cookies=""):
         self.url = url
+        self.Cookies = cookies
         self.session = requests.session()
 
     def get_pic(self):
@@ -30,12 +36,17 @@ class KuaiPic(object):
         pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', re.S)
         deal_url = re.findall(pattern, self.url)[0]
         try:
-            # 获取第一次重定向
-            response = self.session.get(url=deal_url, headers=headers, timeout=10)
-            # 获取第二次重定向
-            result = self.session.get(url=response.url, headers=headers, timeout=10)
+            if "/u/" in deal_url:
+                api = deal_url
+                headers["Cookie"] = self.Cookies
+            else:
+                # 获取第一次重定向
+                response = self.session.get(url=deal_url, headers=headers, timeout=10)
+                # 获取第二次重定向
+                result = self.session.get(url=response.url, headers=headers, timeout=10)
+                api = result.url
             # 从html上解析图片
-            html5 = self.session.get(url=result.url, headers=headers, timeout=10)
+            html5 = self.session.get(url=api, headers=headers, timeout=10)
             if html5.status_code == 200:
                 doc = pq(html5.text)
                 author = doc(".profile-user-name").text()
@@ -58,5 +69,14 @@ class KuaiPic(object):
 
 
 if __name__ == '__main__':
-    kuai_pic = KuaiPic("手段上的还是 https://v.kuaishou.com/cYRkmv 速度加速度就爱看")
-    print(kuai_pic.get_pic())
+    description = """
+使用说明：
+    存在两个参数(url，cookie)：
+       - url表示带待传入链接，可以为APP分享或Web端
+       - cookie可默认不传入，若为Web端链接，则必须传入，因为必须要登录才能获取正确的信息
+    """
+    print(description)
+    ki_pic = KuaiPic(
+        url="https://live.kuaishou.com/u/mx278516/3xahgx5ya7u366a?did=web_088cf239f6654844bce30eb5e3c3abca",
+        cookies="your cookie")
+    print(ki_pic.get_pic())
